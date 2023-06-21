d6801a7ce805e488fccfc9e5f6dc0e1bdcc3df21
Mohamedhany99 <Mohamed.hany182@outlook.com>
2023-06-21 14:24:18 +0300

    adding add usercat and order models and register it to admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Product,UserCart,Order
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(UserCart)
+admin.site.register(Order)
--- api/models.py
+++ api/models.py
@@ -7,6 +7,51 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 # Create your models here.
 #customized user model
 
-
+#task number 3
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.user.username}"
+    
+    class Meta:
+        verbose_name = "user profile"
+        verbose_name_plural = "user profiles"
+        db_table = "user_profiles"
+
+#task number 6
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "product"
+        verbose_name_plural = "products"
+        db_table = "products"
+    
+class UserCart(models.Model):
+    user = models.ForeignKey(UserProfile,on_delete=models.CASCADE,null=True)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self) -> str:
+        return f"{self.user}"
+    
+    class Meta:
+        verbose_name = "user cart"
+        verbose_name_plural = "user carts"
+        db_table = "user_cart"
+
+class Order(models.Model):
+    cart = models.ForeignKey(UserCart,on_delete=models.CASCADE,null=True)
+    total = models.DecimalField(max_digits=8,decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.total}"
+    
+    class Meta:
+        verbose_name = "order"
+        verbose_name_plural = "orders"
+        db_table = "orders"
