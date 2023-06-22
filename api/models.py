547caf3e0cbfdd2719f3a89fc975294b198bccd8
Mohamedhany99 <Mohamed.hany182@outlook.com>
2023-06-22 20:32:36 +0300

    adding flake8 and apply its fixed and suggestion

--- api/models.py
+++ api/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from uuid import uuid4
 from django.contrib.auth.models import User
-
-from django.contrib.auth.validators import UnicodeUsernameValidator
-
 # Create your models here.
 # customized user model
 
